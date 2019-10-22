ed310116df53fc5be300419be35befcdb0e3de45
DanielSzpunar <danielszpunar@gmail.com>
2019-10-22 18:54:11 -0400

    exercise 1

--- script/objectPracticeOne.js
+++ script/objectPracticeOne.js
@@ -17,5 +17,16 @@ const displayInfo = (player) => {
     let output = `Name: ${player.name} <br/>
     Age: ${player.age}`
     document.getElementById("playerInfo").innerHTML = output;
+}
+function playerConstructor (name, age) {
+    this.name = name;
+    this.age = age;
+}
+function createNewPlayer () {
+    let name = document.getElementById("nameInput").value;
+    let age = document.getElementById("ageInput").value;
+    const newPlayer = new playerConstructor(name, age);
     
+    document.getElementById("newPlayer").innerHTML = `New Player: ${newPlayer.name}<br>age: ${newPlayer.age}`
+
 }

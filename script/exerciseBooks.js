43d2ba4f21c3690fe41be30d2cb3a24f460ea8da
DanielSzpunar <danielszpunar@gmail.com>
2019-10-22 22:16:19 -0400

    lesson on prototypes

---
+++ script/exerciseBooks.js
@@ -0,0 +1,38 @@
+//Exercise 1 Book constructor:
+function book(title, author, pages, wasRead) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.wasRead = false
+    this.info = function() {
+        return (`${this.title}, by ${this.author}, ${this.pages} pages, read: ${this.wasRead}`);
+    }
+}
+
+function Plant() {
+    this.country = "Mexico"
+    this.isOrganic = true;
+}
+
+Plant.prototype.showNameAndColor = function () {
+    console.log("I am a " + this.name + " and my color is " + this.color)
+}
+Plant.prototype.AmIOrganic = function() {
+    if (this.isOrganic) {
+        console.log("I am organic baby!");
+    }
+}
+function Fruit(fruitName, fruitColor) {
+    this.name = fruitName
+    this.color = fruitColor;
+}
+Fruit.prototype = new Plant();
+
+
+// Creates a new object, aBanana, with the Fruit constructor
+var aBanana = new Fruit ("Banana", "Yellow");
+// Here, aBanana uses the name property from the aBanana object prototype, which is Fruit.prototype:
+console.log(aBanana.name); // Banana
+
+// Uses the showNameAndColor method from the Fruit object prototype, which is Plant.prototype. The aBanana object inherits all the properties and methods from both the Plant and Fruit functions.
+console.log(aBanana.showNameAndColor()); // I am a Banana and my color is yellow.

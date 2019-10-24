cdaaeebb7c9b016306d235abb27c4afbb33f4615
DanielSzpunar <danielszpunar@gmail.com>
2019-10-23 20:37:49 -0400

    understanding prototypes

--- script/exerciseBooks.js
+++ script/exerciseBooks.js
@@ -14,7 +14,7 @@ function Plant() {
     this.isOrganic = true;
 }
 
-Plant.prototype.showNameAndColor = function () {
+Plant.prototype.showNameAndColor = function() {
     console.log("I am a " + this.name + " and my color is " + this.color)
 }
 Plant.prototype.AmIOrganic = function() {
@@ -36,3 +36,16 @@ console.log(aBanana.name); // Banana
 
 // Uses the showNameAndColor method from the Fruit object prototype, which is Plant.prototype. The aBanana object inherits all the properties and methods from both the Plant and Fruit functions.
 console.log(aBanana.showNameAndColor()); // I am a Banana and my color is yellow.
+
+function Vegetable() {
+    this.origin = "Mexico"
+    this.isOrganic = true;
+}
+Vegetable.prototype.showOriginAndOrganic = function() {
+    return `this plant originates from ${this.origin}. Organic: ${this.isOrganic}`
+}
+function sweetVegetable(name, color) {
+    this.name = name;
+    this.color = color;
+}
+sweetVegetable.prototype = new Vegetable();
